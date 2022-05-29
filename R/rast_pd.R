f46048b33cf0b7bda6025d063b4a7a03969be1f2
Gabriela Alves Ferreira <gaferreira@uesc.br>
2022-05-29 13:45:46 -0300

    description updated
    testthat excluded

--- R/rast_pd.R
+++ R/rast_pd.R
@@ -10,15 +10,15 @@
 rast_pd <- function(pres_bin_stack, tree){
   {
     spatial_names <- as.character(names(pres_bin_stack))   # nomes espaciais e nomes filogeneticos
-    tree_phy4 <- phylo4(tree) # criei essa arvore de classe phylo4 para poder extrair as tipLabels
-    labels <- as.character(tipLabels(tree_phy4))
+    tree_phy4 <- phylobase::phylo4(tree) # criei essa arvore de classe phylo4 para poder extrair as tipLabels
+    labels <- as.character(phylobase::tipLabels(tree_phy4))
     on_tree <- intersect(spatial_names,labels)   # especies do raster que estao na arvore
-    subtree <- keep.tip(tree, on_tree) # arvore com o subconjunto de especies
+    subtree <- ape::keep.tip(tree, on_tree) # arvore com o subconjunto de especies
     stack_reord <- pres_bin_stack[[subtree[["tip.label"]]]] # reordenar o stack de especies de acordo com a ordem das especies na arvore
-    stack_reord_t <- rast(stack_reord) # lendo como um spatraster para se adequar ao pacote Terra
+    stack_reord_t <- terra::rast(stack_reord) # lendo como um spatraster para se adequar ao pacote Terra
     species_name <- names(stack_reord)
-    subtree <- phylo4(subtree)
-    branch_length <- as.numeric(edgeLength(subtree, 1:nTips(subtree)))
+    subtree <- phylobase::phylo4(subtree)
+    branch_length <- as.numeric(phylobase::edgeLength(subtree, 1:phylobase::nTips(subtree)))
   }
   vec_pd <- function(pres_bin_stack, branch_length, species_names){
     pres_bin_stack[is.na(pres_bin_stack)] <- 0 # atribui 0 a tudo que eh NA
@@ -33,6 +33,6 @@ rast_pd <- function(pres_bin_stack, tree){
     return(c(blt = blt, blr = blr)) # retornando os objetos
   }
 
-  rpd <- app(terra:::rast(stack_reord), fun = vec_pd, branch_length = branch_length, species_name = species_name)
+  rpd <- terra::app(terra::rast(stack_reord), fun = vec_pd, branch_length = branch_length, species_name = species_name)
   return(rpd)
 }
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(phylogrid)
-
-test_check("phylogrid")
