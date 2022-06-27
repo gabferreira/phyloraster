2c1072d936fea3ab1cf278e2af08ab1ed990e8f3
Gabriela Alves Ferreira <gaferreira@uesc.br>
2022-06-27 11:48:02 -0300

    function updated

--- R/phylo.pres.R
+++ R/phylo.pres.R
@@ -1,7 +1,7 @@
 #' Reorder stack according to tree order and get branch length
 #'
 #' Reorder stack according to tree order and get branch length.
-#' @param pres.stack a raster of presence-absence. It can be an object of class 'raster' or 'SpatRaster'
+#' @param pres.stack a raster of presence-absence of class 'SpatRaster'
 #' @param tree an object of class 'phylo'
 #'
 #' @return SpatRaster and numeric vector
@@ -15,8 +15,7 @@ phylo.pres <- function(pres.stack, tree){
   on.tree <- intersect(spat.names,labels)   # raster species that are on the tree
   subtree <- ape::keep.tip(tree, on.tree)
 
-  stack.reord <- pres.stack[[subtree[["tip.label"]]]] # reorder the species stack according to the species order in the tree
-  pres.reord <- terra::rast(stack.reord) # SpatRaster
+  pres.reord <- pres.stack[[subtree[["tip.label"]]]] # reorder the species stack according to the species order in the tree
   #names(pres.reord) == subtree[["tip.label"]] # to check wether names match
   subtree <- phylobase::phylo4(subtree)
   branch.length <- as.numeric(phylobase::edgeLength(subtree, 1:phylobase::nTips(subtree)))
