ce50f6c9fa732e81348a107110d820737a0120ea
Gabriela Alves Ferreira <gaferreira@uesc.br>
2022-06-29 08:03:01 -0300

    data deleted

--- R/datar.R
+++
@@ -1,7 +0,0 @@
-#' Presence absence of 33 tree frog species
-#'
-#' A dataset containing values of presence absence of 33 tree frog species.
-#'
-#' @format A matrix with 2891 rows and 33 columns:
-#' @source Rosauer, Dan. 2017. Link: https://github.com/DanRosauer/phylospatial
-"datar"
