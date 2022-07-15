553da960200f641ec7d722654690341887422f7a
Gabriela Alves Ferreira <gaferreira@uesc.br>
2022-07-15 12:32:34 -0300

    new dataset inserted. This data containing a data frame with presence absence data

---
+++ R/dataR.R
@@ -0,0 +1,9 @@
+#' Presence-absence of 33 Australian tree frogs (Rosauer 2017)
+#'
+#' A dataset containing presence-absence of 33 Australian tree frogs. This dataset also provide coordinates x and y for each site.
+#'
+#' @format A matrix with 2891 rows and 33 columns.
+#' @source Rosauer, 2017. Available on: <https://github.com/DanRosauer/phylospatial/tree/master/PhyloEndemism_in_R/Tree%20Frog%20Data>
+"dataR"
+
+
