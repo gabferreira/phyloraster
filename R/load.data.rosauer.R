8485986931f5ed7244c9404c20bfe8702942507d
Gabriela Alves Ferreira <gaferreira@uesc.br>
2022-08-31 14:52:37 -0300

    explanation about funciton uptadet

--- R/load.data.rosauer.R
+++ R/load.data.rosauer.R
@@ -1,16 +1,13 @@
 #' Load an example dataset with presence-absence data of 33 tree frogs and a phylogenetic tree for this species
 #'
-#' This function load a phylogenetic tree and a dataset with presence-absence of 33 Australian tree frogs from Rosauer (2017). Also provides coordinates to each site.
-#'
-#' @param x a data.frame with coordinates and presence-absence data, and a phylogenetic tree.
+#' This function load a phylogenetic tree and a dataset with presence-absence of
+#' 33 Australian tree frogs from Rosauer (2017). Also provides coordinates to each site.
 #'
 #' @return data.frame and phylo
-#' @export
 #' @source Rosauer, 2017. Available on: <https://github.com/DanRosauer/phylospatial/tree/master/PhyloEndemism_in_R/Tree%20Frog%20Data>
 #'
-# #' @examples
-#'
-load.data.rosauer <- function(x){
+#' @export
+load.data.rosauer <- function(){
   x <- phylogrid::dataR
   tree <- ape::read.tree(system.file("extdata", "tree.nex", package="phylogrid"))
   return(list(presab = x, tree = tree))
