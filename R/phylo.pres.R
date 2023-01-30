72cff34c24ba15fd501d56f8dffe335533a70c02
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-01-30 09:12:29 -0300

    description updated

--- R/phylo.pres.R
+++ R/phylo.pres.R
@@ -1,9 +1,10 @@
 #' Prepare rasters and phylogenetic tree to run community metrics
 #'
-#' @description Reorder stack according to tree order and get branch length for each species.
+#' @description Reorder stack according to tree order and get branch length for each species. The names must be the same in the phylogenetic tree and in the raster for the same species. For example, if you have the name "Leptodactylus_latrans" in the raster and "Leptodactylus latrans" in the tree, the function will not work. The same goes for uppercase and lowercase letters.
 #' @param x SpatRaster. A stack containing binary presence-absence rasters for each species.
 #' @param tree phylo. A dated tree.
-#' @return SpatRaster, phylo and numeric vector
+#' @param ... additional arguments to be passed passed down from a calling function.
+#' @return Returns a list containing a SpatRaster reordered according to the order that the species appear in the phylogenetic tree, a subtree containing only the species that are in the stack of rasters and finally two named numerical vectors containing the branch length and the number of descendants of each species.
 #' @author Neander Marcel Heming and Gabriela Alves Ferreira
 #' @export
 #' @examples
@@ -13,12 +14,13 @@
 #' phylo.pres(ras, tree)
 #' }
 
-phylo.pres <- function(x, tree) {
+phylo.pres <- function(x, tree, ...) {
 
   # if(ape::is.rooted(tree) == FALSE){
   #   warning("A rooted phylogeny is required for meaningful output of this function",
   #           call. = FALSE)
   # }
+
   spat.names <- as.character(names(x)) # to extract species names in the raster
   tree.phy4 <- phylobase::phylo4(tree) # transform phylo in phylo4 class
   labels <- as.character(phylobase::tipLabels(tree.phy4)) # extracting species names in the tree
@@ -37,12 +39,12 @@ phylo.pres <- function(x, tree) {
   # Get branch length
   branch.length <- as.numeric(phylobase::edgeLength(subtree,
                                                     1:phylobase::nTips(subtree))) # extract branch lengths
-  names(branch.length) <- names(x) # add names
+  names(branch.length) <- subtree@label  # add names
 
   # Get descendant node numbers
   n.descen <- as.numeric(phylobase::ancestor(subtree,
                                              1:phylobase::nTips(subtree)))
-  names(n.descen) <- labels # add names
+  names(n.descen) <- subtree@label # add names
 
   pp <- list(x = x, branch.length = branch.length, n.descendents = n.descen, subtree = subtree)
   return(pp)
