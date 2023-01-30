9f8b62b6564e1f9319367d338dfaaaefc8ea7d1d
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-01-30 16:40:18 -0300

    function shp2rast has been updated with new arguments

--- tests/testthat/test-class-shp2rast.R
+++ tests/testthat/test-class-shp2rast.R
@@ -3,7 +3,7 @@ test_that("check if the object class is correct", {
   # load data
   shp <- terra::vect(system.file("extdata", "shps_iucn_spps_rosauer.shp", package="phylogrid"))
   spps <- shp$BINOMIAL
-  sr <- phylogrid::shp2rast(shp, spps, resolution = 0.1)
+  sr <- shp2rast(shp, sps.col = "BINOMIAL", ymask = FALSE, background = 0, resolution = 0.5)
 
   # tests
   expect_s4_class(sr, "SpatRaster")
