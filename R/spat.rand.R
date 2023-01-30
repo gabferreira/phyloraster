21c31dbaff0312fe9fe8ec8420aab76549ecbaa4
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-01-30 09:13:17 -0300

    documentation for intern function

--- R/spat.rand.R
+++ R/spat.rand.R
@@ -16,12 +16,25 @@
   }, fr = fr)
 }
 
+#' Intern function to sample vectors with non-NA values
+#'
+#' @param x  numeric. A vector containing values to resampling.
+#'
+#' @return vector
+# #' @export
+#'
+#' @examples
+.sample.not.NA <- function(x){
+  s <- !is.na(x)
+  x[s] <- sample(x[s])
+  x
+}
+
 #' Randomize a set of rasters according to the observed frequency.
 #'
 #' Randomize a set of rasters according to the observed frequency using the methods: sites (by cells), species (by layer) or both (layers and cells).
-#'
+#' @param cores positive integer. If cores > 1, a 'parallel' package cluster with that many cores is created and used.
 #' @param x SpatRaster. A presence-absence SpatRaster.
-#' @param aleats numeric. A number indicating how many times the calculation should be repeated.
 #' @param random character. Character indicating the type of randomization to be used. The available types are by "site", "specie" or "fullspat".
 #' @return SpatRaster
 #' @author Neander Marcel Heming and Gabriela Alves-Ferreira
@@ -29,53 +42,68 @@
 #'
 #' @examples
 #' \dontrun{
-#' aleats = 10
 #' ras <- terra::rast(system.file("extdata", "rast.presab.tif", package="phylogrid"))
-#' sr <- spat.rand(ras, aleats = 10, random = "site")
+#' sr <- spat.rand(ras, random = "site")
 #' plot(sr)
 #' }
-spat.rand <- function(x, aleats, random = c("site", "specie", "fullspat")){
+spat.rand <- function(x, random = c("site", "species", "fullspat"),
+                      filename = "", memory = FALSE, cores = 1){
 
-  aleats <- aleats # number of null models
   fr <- terra::freq(x)
 
-  if (random == "site"){
+  if (random == "species"){
+    if(memory){ # quando cabe na memoria
 
-    resu <- list()
-
-    for(i in 1:aleats){
       # randomize by layers- sites
-      resu[[i]] <- terra::rast(lapply(1:terra::nlyr(x),
-                                      function(i, r, fr){
-                                        terra::app(r[[i]], fun=.lyr.sample, fr=fr[fr$layer==i,])
-                                      }, r = x, fr = fr))
+      resu <- terra::rast(lapply(1:terra::nlyr(x),
+                                 function(i, r){
+                                   terra::app(r[[i]],
+                                              fun = .sample.not.NA)
+                                 }, r = x))
+    } else { # nao cabe na memoria
+
+      # randomize by layers- sites
+      resu <- terra::writeRaster(terra::rast(lapply(1:terra::nlyr(x),
+                                                    function(i, r, fr){
+                                                      terra::app(r[[i]], fun = .lyr.sample,
+                                                                 fr = fr[fr$layer==i,])
+                                                    }, r = x, fr = fr)),
+                                 filename = filename)
+
+
     }
-    resu <- terra::rast(resu) # to transform a list in raster
 
-  } else if (random == "specie") {
+  } else if (random == "site") {
 
-    resu <- list()
-
-    for(i in 1:aleats){
+    if(memory){
       ### randomize by cells- species in each site
-      resu[[i]] <- terra::app(x, sample)
+      resu <- terra::app(x, sample, cores = cores, filename = filename)
+
+    } else {
+      ### randomize by cells- species in each site
+      resu <- terra::app(x, sample, cores = cores, filename = filename)
+
     }
-    resu <- terra::rast(resu) # to transform a list in raster
 
   } else if (random == "fullspat") {
 
-    resu <- list()
-
-    for(i in 1:aleats){
+    if(memory){
       ### randomize by sites and species!
-      resu[[i]] <- terra::app(x, fun = .lyr.sample, fr = fr)
+      resu <- x
+      resu[] <- .sample.not.NA(x[])
+
+    } else {
+      ### randomize by sites and species!
+      resu <- terra::app(x, fun = .lyr.sample, fr = fr, cores = cores,
+                         filename = filename)
+
     }
-    resu <- terra::rast(resu) # to transform a list in raster
 
   } else {
-    stop("Choose a valid randomization method! The methods currently available are: 'site', 'specie', 'fullspat'.")
+    stop("Choose a valid randomization method! The methods currently available are: 'site', 'species', 'fullspat'.")
   }
 
   return(resu)
 
 }
+
