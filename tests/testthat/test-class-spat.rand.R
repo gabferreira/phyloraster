c2fd4e0a5b64685ebb5b2bb9b9934be8fc544fd6
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-01-30 16:41:18 -0300

    argument aleats has been removed of the function

--- tests/testthat/test-class-spat.rand.R
+++ tests/testthat/test-class-spat.rand.R
@@ -2,7 +2,7 @@ test_that("check if the object class is correct", {
 
   # load data
   x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phylogrid"))
-  sr <- phylogrid::spat.rand(x, aleats = 5, random = "site")
+  sr <- phylogrid::spat.rand(x, random = "site")
 
   # tests
   expect_s4_class(sr, "SpatRaster")
