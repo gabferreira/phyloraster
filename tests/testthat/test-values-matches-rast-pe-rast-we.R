d822890e756c2b2ab0f2c2a2c89c1c269757bf24
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-04-18 07:29:11 -0300

    new test added to evaluate if the PE function returns the same result as WE when the tree is a rake (the branch lenght of any species is 1)

---
+++ tests/testthat/test-values-matches-rast-pe-rast-we.R
@@ -0,0 +1,19 @@
+test_that("PE function returns the same result as WE when the tree is a rake", {
+
+  require(glottoTrees)
+
+  x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phylogrid"))
+  tree <- ape::read.tree(system.file("extdata", "tree.nex", package="phylogrid"))
+
+  # setting all branches to length equal to one
+  tree2 <- glottoTrees::rescale_branches(tree, length = 1)
+  data <- phylogrid::phylo.pres(x, tree2)
+
+  # calculating WE and PE with all branch lengths = 1
+  we <- phylogrid::rast.we(x)
+  pe <- phylogrid::rast.pe(data$x, data$branch.length)
+
+  # testing if the values are the same for WE and PE
+  expect_equivalent(terra::values(we), terra::values(pe))
+
+})
