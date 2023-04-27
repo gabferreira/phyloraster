83289e25102f88610c86f3d39da159676f63e8f2
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-04-27 09:09:37 -0300

    function deleted

--- tests/testthat/test-class-spat.rand.R
+++
@@ -1,9 +0,0 @@
-test_that("check if the object class is correct", {
-
-  # load data
-  x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phylogrid"))
-  sr <- phylogrid::spat.rand(x, random = "site")
-
-  # tests
-  expect_s4_class(sr, "SpatRaster")
-})
