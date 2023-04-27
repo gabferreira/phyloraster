be1686553fce4d3e6e825f9269e25334ab7cd6fe
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-04-27 09:12:41 -0300

    phylogrid to phyloraster

--- tests/testthat/test-values-delta-grid.R
+++ tests/testthat/test-values-delta-grid.R
@@ -1,6 +1,6 @@
 test_that("Are the returned values correct?", {
 
-  x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phylogrid"))
+  x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phyloraster"))
 
   # getting fewer cells to test all values
   r <- terra::rast()
@@ -8,9 +8,9 @@ test_that("Are the returned values correct?", {
   xcrop <- terra::crop(x, terra::ext(r))
 
   # metric SE richness
-  riq.pres <- phylogrid::rast.se(xcrop)
-  riq.fut <- phylogrid::rast.se(xcrop[[c(1:9)]]) # imagine we lost some species in the future
-  dg.obs <- terra::values(phylogrid::delta.grid(riq.pres, riq.fut))
+  riq.pres <- phyloraster::rast.se(xcrop)
+  riq.fut <- phyloraster::rast.se(xcrop[[c(1:9)]]) # imagine we lost some species in the future
+  dg.obs <- terra::values(phyloraster::delta.grid(riq.pres, riq.fut))
   c(dg.obs)
   dg.expect <- matrix(data = c(-8,  -8,  -8,  -9, -10, -10, -10, -10,  -8,  -8,
                                -8,  -8,  -9, -10, -10, -10), ncol = 1, byrow = F)
