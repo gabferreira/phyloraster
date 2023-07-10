34dee30e521695af95369d3728f4490bee821403
Gabriela Alves Ferreira <gaferreira@uesc.br>
2023-07-10 17:18:16 -0300

    help updated and argument unit added

--- R/range_size.R
+++ R/range_size.R
@@ -1,24 +1,24 @@
 #' Calculate range size for a set of species using a raster as imput
 #'
-#' @description This function calculate range size in square kilometers for all cells that are not NA. The size of the cells is constant in degrees but not in square meters, which was considered in the method applied to calculate the area. If scale is TRUE, the raster is scaled from 0 to 1.
+#' @description This function calculate range size  in meters (by default) for all cells that are not NA. The size of the cells is constant in degrees but not in square meters, which was considered in the method applied to calculate the area.
 #' @param x SpatRaster. A SpatRaster containing presence-absence data (0 or 1) for a set of species. This raster must be named.
-#' @param scale logical. If TRUE, scaling is done by dividing the range size x by the area total. If FALSE, scaling is not done. The default is FALSE.
 #' @param ... additional arguments to be passed passed down from a calling function.
+#' @inheritParams terra::cellSize
 #' @author Gabriela Alves Ferreira and Neander Marcel Heming
 #' @return vector
 #' @export
 #' @examples
 #' \dontrun{
 #' x <- terra::rast(system.file("extdata", "rast.presab.tif", package="phyloraster"))
-#' range_size(x, scale = TRUE)
+#' range_size(x)
 #' }
-range_size <- function(x, scale = FALSE, ...){
+range_size <- function(x, unit = "m", ...){
 
   # colocar ifelse do mi
   temp <- vector("list", length = 2) # to create a temporary vector with the raster number
   temp[[1]] <- paste0(tempfile(), ".tif")  # to store the first raster
 
-  area <- terra::cellSize(terra::rast(x[[1]]), filename = temp[[1]], unit="km") # to calculate cell size
+  area <- terra::cellSize(terra::rast(x[[1]]), filename = temp[[1]], unit = unit) # to calculate cell size
 
   # The function bellow extracts the range size for each species and stores it in a vector
   rs <- sapply(1:terra::nlyr(x),
